1870e4e474785b56c956cecb25821c7258530531
Marwa Selwaye <75786866+Marwasel@users.noreply.github.com>
2023-11-13 11:34:28 +0100

    Ui screens adjustments

--- composeApp/src/commonMain/kotlin/screens/SignupScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/SignupScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -19,7 +21,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -41,7 +45,8 @@ fun SignUpScreen( onSignupButtonClick: () -> Unit,
 
     Column(
         modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         Text(
@@ -49,7 +54,7 @@ fun SignUpScreen( onSignupButtonClick: () -> Unit,
             fontSize = 40.sp,
             textAlign = TextAlign.Center,
             fontWeight = FontWeight.Bold,
-            color = Color(0xFF6A1B9A),
+            color =  Color(0xFF926EB4),
             modifier = Modifier.fillMaxWidth().padding(vertical = 32.dp)
         )
 
@@ -103,10 +108,12 @@ fun SignUpScreen( onSignupButtonClick: () -> Unit,
         // sign-up button
         Button(
             onClick = { onSignupButtonClick.invoke() },
-            modifier = Modifier.fillMaxWidth()
-                .padding(top = 16.dp),
+            modifier = Modifier
+                .width(200.dp)
+                .padding(top = 8.dp, bottom = 8.dp)
+                .clip(RoundedCornerShape(percent = 50)),
             colors = ButtonDefaults.buttonColors(
-                backgroundColor = Color(0xFF6A1B9A),
+                backgroundColor =  Color(0xFF926EB4),
             ),
             elevation = ButtonDefaults.elevation(
                 defaultElevation = 0.dp,
@@ -128,7 +135,7 @@ fun SignUpScreen( onSignupButtonClick: () -> Unit,
                 .padding(top = 16.dp),
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = Color.Transparent,
-                contentColor = Color(0xFF6A1B9A),
+                contentColor =  Color(0xFF926EB4),
             ),
             elevation = ButtonDefaults.elevation(
                 defaultElevation = 0.dp,
@@ -138,8 +145,9 @@ fun SignUpScreen( onSignupButtonClick: () -> Unit,
         ) {
             Text(
                 text = "Go back",
+                fontSize = 12.sp,
                 fontWeight = FontWeight.Bold,
-                color = Color(0xFF6A1B9A)
+                color =  Color(0xFF926EB4),
             )
         }
     }
